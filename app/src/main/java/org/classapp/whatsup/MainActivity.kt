e557bbda271c9a44bc6a1a4762b4e68db0ee0069
Pong50887 <pichayoot.pong@gmail.com>
2025-02-04 20:12:53 +0700

    Done Class 04

--- app/src/main/java/org/classapp/whatsup/MainActivity.kt
+++ app/src/main/java/org/classapp/whatsup/MainActivity.kt
@@ -72,6 +72,7 @@ fun MainScreenWithBottomNavBar() {
                     icon = { Icon( imageVector = itemInfo.icon, contentDescription = itemInfo.label )},
                     label = { Text(text = itemInfo.label) })
             }
+            
         }
     }) { paddingValues -> NavHost(navController = navController,
         startDestination = DestinationScreens.Highlight.route,
